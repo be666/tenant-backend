6f4c983410a1f1ecc09a0986296eed392c106831
bcaring <bcaring@163.com>
2015-11-19 21:03:13 +0800

    code

---
+++ src/main/webapp/asset/javascript/06-controller/code.js
@@ -0,0 +1,20 @@
+/**
+ * Created by bcaring on 15/11/18.
+ */
+define('controller/code', ['service/code_service', "template"], function (require, exports, module) {
+
+    exports.layout = function (codeLayout, codes, currentStatus, serviceType, orgType, schoolType, courseType) {
+
+    };
+
+
+    exports.query = function (pId) {
+        iMethod._.ajax({
+            url: "",
+            success: function () {
+
+            }
+        })
+    }
+    iMethod.controller.code = module.exports;
+});
